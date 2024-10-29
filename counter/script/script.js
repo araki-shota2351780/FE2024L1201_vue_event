f26eacaf8bb112dcf40e07cf56013afda71699ff
araki-shota2351780 <araki.shota0227@gmail.com>
2024-10-29 01:03:35 +0000

    change

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,25 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count: 1,
+            num: 0
         };
+    },
+    methods: {
+        kakeru() {
+            this.count =  this.count *2;
+        },
+        waru() {
+            this.count = this.count /2;
+        },
+        tasu() {
+            this.count = this.count + this.num;
+        },
+        hiku() {
+            this.count = this.count - this.num;
+        },
+        reset() {
+            this.count = 1;
+        }
     }
 });
